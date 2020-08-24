27e0428c33c24c651d71474654f8b9836b2153a2
APP技術部-陳冠志 <bruce@mitake.com.tw>
2020-08-24 10:16:05 +0800

    Initial Commit

---
+++ TestMenuBae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMenuBae
+//
+//  Created by APP技術部-陳冠志 on 2020/8/24.
+//  Copyright © 2020 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
